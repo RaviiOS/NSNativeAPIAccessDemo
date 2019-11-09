92c9248d7edcf5fe8694d5bdbb3e57d1184fb363
Ravi <yaganti.ravikumar@gmail.com>
2019-11-10 00:24:34 +0530

    Updating UI Moved BLE Functionality to home component

--- MySampleApp/src/app/app.component.ts
+++ MySampleApp/src/app/app.component.ts
@@ -4,7 +4,6 @@ import { RouterExtensions } from "nativescript-angular/router";
 import { DrawerTransitionBase, RadSideDrawer, SlideInOnTopTransition } from "nativescript-ui-sidedrawer";
 import { filter } from "rxjs/operators";
 import * as app from "tns-core-modules/application";
-import { HomeComponent } from "./home/home.component";
 
 @Component({
     selector: "ns-app",
@@ -13,34 +12,12 @@ import { HomeComponent } from "./home/home.component";
 export class AppComponent implements OnInit {
     private _activatedUrl: string;
     private _sideDrawerTransition: DrawerTransitionBase;
-    centralManager: CBCentralManager;
-    bleDelegte: CBLEManagerDelegateImplement;
 
     constructor(private router: Router, private routerExtensions: RouterExtensions) {
         // Use the component constructor to inject services.
-        this.centralManager = CBCentralManager.new();
-        this.bleDelegte = CBLEManagerDelegateImplement.initWithOwner(new WeakRef(this));
-        this.centralManager = this.centralManager.initWithDelegateQueue(this.bleDelegte, null);
-        console.log("Is scannng  ====> ",this.centralManager.state);
+
     }
 
-    bluetoothStatus(central: CBCentralManager) {
-            switch (central.state) {
-                case CBManagerState.PoweredOff:
-                        console.log("Discovered BLE State PoweredOff ===>", central.state);
-                case CBManagerState.PoweredOn:
-                        console.log("Discovered BLE State PoweredOn ===>", central.state);
-                case CBManagerState.Resetting:
-                        console.log("Discovered BLE State Resetting ===>", central.state);
-                case CBManagerState.Unauthorized:
-                        console.log("Discovered BLE State Unauthorized  ===>", central.state);
-                case CBManagerState.Unknown:
-                        console.log("Discovered BLE State Unknown ===>", central.state);
-                case CBManagerState.Unsupported:
-                        console.log("Discovered BLE State Unsupported fromcomponent ===>", central.state);
-        
-            }
-    }
     ngOnInit(): void {
         this._activatedUrl = "/home";
         this._sideDrawerTransition = new SlideInOnTopTransition();
@@ -70,45 +47,3 @@ export class AppComponent implements OnInit {
     }
 }
 
-
-class CBLEManagerDelegateImplement extends NSObject implements CBCentralManagerDelegate {
-    centralManagerConnectionEventDidOccurForPeripheral?(central: CBCentralManager, event: CBConnectionEvent, peripheral: CBPeripheral): void {
-        throw new Error("Method not implemented.");
-    }
-    centralManagerDidConnectPeripheral?(central: CBCentralManager, peripheral: CBPeripheral): void {
-        throw new Error("Method not implemented.");
-    }
-    centralManagerDidDisconnectPeripheralError?(central: CBCentralManager, peripheral: CBPeripheral, error: NSError): void {
-        throw new Error("Method not implemented.");
-    }
-    centralManagerDidDiscoverPeripheralAdvertisementDataRSSI?(central: CBCentralManager, peripheral: CBPeripheral, advertisementData: NSDictionary<string, any>, RSSI: number): void {
-        throw new Error("Method not implemented.");
-    }
-    centralManagerDidFailToConnectPeripheralError?(central: CBCentralManager, peripheral: CBPeripheral, error: NSError): void {
-        throw new Error("Method not implemented.");
-    }
-    centralManagerDidUpdateANCSAuthorizationForPeripheral?(central: CBCentralManager, peripheral: CBPeripheral): void {
-        throw new Error("Method not implemented.");
-    }
-    centralManagerDidUpdateState(central: CBCentralManager): void {
-        this._owner.get().bluetoothStatus(central);
-    }
-    centralManagerWillRestoreState?(central: CBCentralManager, dict: NSDictionary<string, any>): void {
-        throw new Error("Method not implemented.");
-    }
-
-    static ObjCProtocols = [CBCentralManagerDelegate] // define our native protocalls
-
-    private _owner: WeakRef<AppComponent>;
-
-    static new(): CBLEManagerDelegateImplement {
-        return <CBLEManagerDelegateImplement>super.new() // calls new() on the NSObject
-    }
-
-    static initWithOwner(owner: WeakRef<AppComponent>) : CBLEManagerDelegateImplement {
-        let delegate = <CBLEManagerDelegateImplement>CBLEManagerDelegateImplement.new();
-        delegate._owner = owner;
-        return delegate;
-    }
-
-}
--- MySampleApp/src/app/home/home.component.ts
+++ MySampleApp/src/app/home/home.component.ts
@@ -1,23 +1,66 @@
 import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
+import { CBLEManagerDelegateImplement } from "../BLE/cble-manager-delegate-implement";
+import { ItemService, Item } from "../BLE/item-service";
+import { ItemEventData } from "tns-core-modules/ui/list-view";
+
+
 
 @Component({
     selector: "Home",
     templateUrl: "./home.component.html"
 })
 export class HomeComponent implements OnInit {
+    centralManager: CBCentralManager;
+    bleDelegte: CBLEManagerDelegateImplement;
+    items: Array<Item>;
 
-    constructor() {
+    constructor(private _itemService: ItemService) {
         // Use the component constructor to inject providers.
     }
 
+    bluetoothStatus(central: CBCentralManager) {
+        switch (central.state) {
+            case CBManagerState.PoweredOff:
+                    console.log("Discovered BLE State PoweredOff ===>", central.state);
+            case CBManagerState.PoweredOn:
+                    console.log("Discovered BLE State PoweredOn ===>", central.state);
+            case CBManagerState.Resetting:
+                    console.log("Discovered BLE State Resetting ===>", central.state);
+            case CBManagerState.Unauthorized:
+                    console.log("Discovered BLE State Unauthorized  ===>", central.state);
+            case CBManagerState.Unknown:
+                    console.log("Discovered BLE State Unknown ===>", central.state);
+            case CBManagerState.Unsupported:
+                    console.log("Discovered BLE State Unsupported fromcomponent ===>", central.state);
+    
+        }
+    }
+    
     ngOnInit(): void {
         // Init your component properties here.
+        this.centralManager = CBCentralManager.new();
+        this.bleDelegte = CBLEManagerDelegateImplement.initWithOwner(new WeakRef(this));
+        this.centralManager = this.centralManager.initWithDelegateQueue(this.bleDelegte, null);
+        console.log("Is scannng  ====> ",this.centralManager.state);
+
+        // To get the dummy data
+        this.items = this._itemService.getItems();
+        console.log("***** Dummy Data ===>", this.items);
     }
 
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
     }
+
+    onSearch() {
+        console.log("**** Tap on on search button ****");
+    }
+
+    onItemTap(args: ItemEventData) {
+        console.log(`Index: ${args.index}; View: ${args.view} ; Item: ${this.items[args.index]}`);
+    }
+
 }
